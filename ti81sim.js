e1f19b461b60ea42e1bbd94b0fc9b13a137cc3e6
mushrom81 <44351080+mushrom81@users.noreply.github.com>
2019-06-16 16:37:26 -0700

    Adapted to an add-on

--- ti81sim.js
+++ ti81sim.js
@@ -1,15 +1,18 @@
-let c = document.querySelector("canvas");
-
-c.width = 96 * 5;
-c.height = 64 * 5;
-
-let ctx = c.getContext("2d");
-
 class TI81Screen {
-    constructor() {
+    constructor(canvas) {
+        this._c = canvas;
+        this._c.width = 96 * 5;
+        this._c.height = 64 * 5;
+        this._ctx = this._c.getContext("2d");
         this._screen = [];
         for (var i = 0; i < 96 * 64; i++) this._screen.push(0);
     }
+
+    clearScreen() {
+        this._screen = [];
+        for (var i = 0; i < 96 * 64; i++) this._screen.push(0);
+    }
+
     pxTest(x, y) {
         if (x < 0 || y < 0 || x > 95 || y > 63) return;
         return this._screen[y * 96 + x];
@@ -31,25 +34,11 @@ class TI81Screen {
     render() {
         for (var y = 0; y < 64; y++) {
             for (var x = 0; x < 96; x++) {
-                if (this._screen[y * 96 + x] == 1) { ctx.fillStyle = "#1b2b17"; }
-                else { ctx.fillStyle = "#aad884"; }
-                ctx.fillRect(x * 5, y * 5, 5, 5);
-                ctx.stroke();
+                if (this._screen[y * 96 + x] == 1) { this._ctx.fillStyle = "#1b2b17"; }
+                else { this._ctx.fillStyle = "#aad884"; }
+                this._ctx.fillRect(x * 5, y * 5, 5, 5);
+                this._ctx.stroke();
             }            
         }
     }
 }
-
-let keys = {};
-onkeydown = onkeyup = function (e) {
-    e = e || window.event;
-    keys[e.key] = (e.type == "keydown");
-}
-
-let ti81 = new TI81Screen();
-
-function loop() {
-    requestAnimationFrame(loop);
-    ti81.render();
-}
-loop();
